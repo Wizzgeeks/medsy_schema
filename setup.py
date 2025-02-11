7d45dc89e241868ee6ebe232f7590f90ff56e96c
naveenkumar <naveenkumar@wizzgeeks.com>
2025-02-11 21:33:02 +0530

    added model for pulse

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="medsy_schema",
-    version="0.4.4",
+    version="0.4.5",
     description="Schema for Flask Admin and User backends",
     author="wizzgeeks",
     author_email="",
