16e2133144309b8ed90153f9c010a16e1372bf02
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-02-13 17:32:10 +0530

    saved notes model

--- Models/save_notes_model.py
+++ Models/save_notes_model.py
@@ -4,11 +4,11 @@ from Models.user_model import User
 from Models.lesson_note_model import Lesson_note
 
 class Saved_notes(Document):
-    user = ReferenceField(User,required=True,reverse_delete_rule=2)
+    user = ReferenceField(User,reverse_delete_rule=2)
     Lesson_note= ReferenceField(Lesson_note,required=True,reverse_delete_rule=2)
     type = StringField(choices=['Revise Later','Important','Reference'],required=True)
 
-    
+
     def to_json(self):
         return {
             "id": str(self.id),
