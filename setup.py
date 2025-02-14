0aa379cddd7181e5d915cbc4c72ebae0046ed96a
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-02-14 10:46:05 +0530

    self notes model

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="medsy_schema",
-    version="0.5.1",
+    version="0.5.2",
     description="Schema for Flask Admin and User backends",
     author="wizzgeeks",
     author_email="",
