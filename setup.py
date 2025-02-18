efddee386686b60ea41a1263a4d79f36f65ba933
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-02-18 14:28:06 +0530

    admin json changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="medsy_schema",
-    version="0.6.8",
+    version="0.6.9",
     description="Schema for Flask Admin and User backends",
     author="wizzgeeks",
     author_email="",
