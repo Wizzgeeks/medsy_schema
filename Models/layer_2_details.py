3b4038ad5484e8489eb956294640a81d98af507c
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-02-21 18:05:25 +0530

    layer details schema created for all the three layers

---
+++ Models/layer_2_details.py
@@ -0,0 +1,14 @@
+from mongoengine import Document,StringField,ReferenceField
+from Models.course_model import Course
+
+
+class Layer_2_details(Document):
+    course = ReferenceField(Course,required=True,reverse_delete_rule=2)
+    layer_2_name = StringField(required=True)
+
+    def to_json(self):
+        return {
+            'course': self.course.key,
+            'layer_1_name': self.layer_2_name
+        }
+            
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="medsy_schema",
-    version="0.8.4",
+    version="0.8.5",
     description="Schema for Flask Admin and User backends",
     author="wizzgeeks",
     author_email="",
