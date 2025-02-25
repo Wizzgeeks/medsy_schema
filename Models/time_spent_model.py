16b100ea39f1f50b6a39e02d53bbe368b0d8fdca
naveenkumar <naveenkumar@wizzgeeks.com>
2025-02-25 17:35:33 +0530

    updated timespent

--- Models/time_spent_model.py
+++ Models/time_spent_model.py
@@ -1,19 +1,20 @@
-from mongoengine import Document,ReferenceField,StringField,ValidationError
+from mongoengine import Document,ReferenceField,StringField,ValidationError,ListField
 from Models.layer_3_model import Layer_3
-from Models.year_model import Year
 from Models.user_model import User
 
 class Time_spent(Document):
-    layer3 = ReferenceField(Layer_3,required=True,reverse_delete_rule=2)
+    layer3 = ReferenceField(Layer_3,reverse_delete_rule=2)
     user = ReferenceField(User,required=True,reverse_delete_rule=2)
-    time_spent = StringField(required=True)
+    time_spent = StringField()
+    active_time = ListField()
 
     def to_json(self):
         return {
             "id":str(self.id),
             "layer3":str(self.layer3.id) if self.layer3 else None,
             "user":str(self.user.id) if self.user else None,
-            "time_spent":self.time_spent
+            "time_spent":self.time_spent,
+            "active_time":self.active_time
         }
     
     def with_key(self):
@@ -21,7 +22,8 @@ class Time_spent(Document):
             "id":str(self.id),
             "layer3":self.layer3.to_json() if self.layer3 else None,
             "user":self.user.to_json() if self.user else None,
-            "time_spent":self.time_spent
+            "time_spent":self.time_spent,
+            "active_time":self.active_time
         }
     
     def clean(self):
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="medsy_schema",
-    version="1.0.6",
+    version="1.0.7",
     description="Schema for Flask Admin and User backends",
     author="wizzgeeks",
     author_email="",
