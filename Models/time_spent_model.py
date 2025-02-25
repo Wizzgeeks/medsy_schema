9d8c8abf7f8f5cb848098829885e9e9e4e83a452
naveenkumar <naveenkumar@wizzgeeks.com>
2025-02-25 18:02:21 +0530

    time spent

--- Models/time_spent_model.py
+++ Models/time_spent_model.py
@@ -6,7 +6,7 @@ class Time_spent(Document):
     layer3 = ReferenceField(Layer_3,reverse_delete_rule=2)
     user = ReferenceField(User,required=True,reverse_delete_rule=2)
     time_spent = StringField()
-    active_time = ListField()
+    # active_time = ListField()
 
     def to_json(self):
         return {
@@ -14,7 +14,7 @@ class Time_spent(Document):
             "layer3":str(self.layer3.id) if self.layer3 else None,
             "user":str(self.user.id) if self.user else None,
             "time_spent":self.time_spent,
-            "active_time":self.active_time
+            # "active_time":self.active_time
         }
     
     def with_key(self):
@@ -23,7 +23,7 @@ class Time_spent(Document):
             "layer3":self.layer3.to_json() if self.layer3 else None,
             "user":self.user.to_json() if self.user else None,
             "time_spent":self.time_spent,
-            "active_time":self.active_time
+            # "active_time":self.active_time
         }
     
     def clean(self):
