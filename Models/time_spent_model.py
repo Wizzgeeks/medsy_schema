217fda2edb209d074f92c73d8379328df846569f
naveenkumar <naveenkumar@wizzgeeks.com>
2025-02-25 12:28:18 +0530

    altered time spent

--- Models/time_spent_model.py
+++ Models/time_spent_model.py
@@ -1,18 +1,9 @@
 from mongoengine import Document,ReferenceField,StringField,ValidationError
-from Models.course_model import Course
-from Models.layer_1_model import Layer_1
-from Models.subject_model import Subject
-from Models.layer_2_model import Layer_2
 from Models.layer_3_model import Layer_3
 from Models.year_model import Year
 from Models.user_model import User
 
 class Time_spent(Document):
-    course = ReferenceField(Course,required=True,reverse_delete_rule=2)
-    year = ReferenceField(Year,required=True,reverse_delete_rule=2)
-    subject = ReferenceField(Subject,required=True,reverse_delete_rule=2)
-    layer1 = ReferenceField(Layer_1,reverse_delete_rule=2)
-    layer2 = ReferenceField(Layer_2,required=True,reverse_delete_rule=2)
     layer3 = ReferenceField(Layer_3,required=True,reverse_delete_rule=2)
     user = ReferenceField(User,required=True,reverse_delete_rule=2)
     time_spent = StringField(required=True)
@@ -20,11 +11,6 @@ class Time_spent(Document):
     def to_json(self):
         return {
             "id":str(self.id),
-            "course":str(self.course.id) if self.course else None,
-            "year":str(self.year.id) if self.year else None,
-            "subject":str(self.subject.id) if self.subject else None,
-            "layer1":str(self.layer1.id) if self.layer1 else None,
-            "layer2":str(self.layer2.id) if self.layer2 else None,
             "layer3":str(self.layer3.id) if self.layer3 else None,
             "user":str(self.user.id) if self.user else None,
             "time_spent":self.time_spent
@@ -33,11 +19,6 @@ class Time_spent(Document):
     def with_key(self):
         return {
             "id":str(self.id),
-            "course":self.course.to_json() if self.course else None,
-            "year":self.year.to_json() if self.year else None,
-            "subject":self.subject.to_json() if self.subject else None,
-            "layer1":self.layer1.to_json() if self.layer1 else None,
-            "layer2":self.layer2.to_json() if self.layer2 else None,
             "layer3":self.layer3.to_json() if self.layer3 else None,
             "user":self.user.to_json() if self.user else None,
             "time_spent":self.time_spent
