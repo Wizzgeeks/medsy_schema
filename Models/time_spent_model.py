9f49b0c5ec4116141cf5df7dc7b7709a39764dae
naveenkumar <naveenkumar@wizzgeeks.com>
2025-02-25 18:13:57 +0530

    time spent

--- Models/time_spent_model.py
+++ Models/time_spent_model.py
@@ -3,10 +3,9 @@ from Models.layer_3_model import Layer_3
 from Models.user_model import User
 
 class Time_spent(Document):
-    layer3 = ReferenceField(Layer_3,reverse_delete_rule=2)
+    layer3 = StringField(required=True)
     user = ReferenceField(User,required=True,reverse_delete_rule=2)
     time_spent = StringField()
-    # active_time = ListField()
 
     def to_json(self):
         return {
@@ -14,7 +13,6 @@ class Time_spent(Document):
             "layer3":str(self.layer3.id) if self.layer3 else None,
             "user":str(self.user.id) if self.user else None,
             "time_spent":self.time_spent,
-            # "active_time":self.active_time
         }
     
     def with_key(self):
@@ -23,7 +21,6 @@ class Time_spent(Document):
             "layer3":self.layer3.to_json() if self.layer3 else None,
             "user":self.user.to_json() if self.user else None,
             "time_spent":self.time_spent,
-            # "active_time":self.active_time
         }
     
     def clean(self):
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="medsy_schema",
-    version="1.0.8",
+    version="1.0.9",
     description="Schema for Flask Admin and User backends",
     author="wizzgeeks",
     author_email="",
