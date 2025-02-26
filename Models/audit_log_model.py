e4b4c5583b49d8bc903e1a2ebc33be84e4e7ced6
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-02-26 12:41:08 +0530

    audit log table created

---
+++ Models/audit_log_model.py
@@ -0,0 +1,23 @@
+from mongoengine import Document,StringField,IntField,ReferenceField,DateTimeField,ListField,DictField
+from Models.user_model import User
+from datetime import datetime
+class Audit_log(Document):
+    user_id = ReferenceField(User,required=True,reverse_delete_rule=2)
+    method=StringField()
+    table_name=StringField()
+    id_or_key=StringField()
+    updated_data=DictField(default={})
+    existing_data=DictField(default={})
+    created_at = DateTimeField(default=datetime.utcnow)
+
+    def to_json(self):
+        return {
+            'user_id': str(self.user_id.id) if self.user_id else None,
+            'method':self.method,
+            'table_name':self.table_name,
+            'id_or_key':self.id_or_key,
+            'updated_data':self.updated_data,
+            'existing_data':self.existing_data,
+            'created_at':self.created_at.isoformat()
+        }
+
