7aea1a9d6b65b48ef2b6c2c17220f53b9ba64160
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-02-27 17:19:20 +0530

    referense field removed in user

--- Models/user_model.py
+++ Models/user_model.py
@@ -7,7 +7,7 @@ class User(Document):
     course =StringField()
     year = StringField()
     username = StringField(required=True)
-    email = EmailField(unique=True,required=False,sparse=True)
+    email = EmailField(unique=True,required=False)
     phone= StringField(unique=True,required=False,sparse=True)
     profile=StringField()
     role=StringField(choices=['student','admin','superadmin'],required=True)
