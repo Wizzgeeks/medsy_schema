e7ac1806a9e8273261e069e408715e39e099b2a4
naveenkumar <naveenkumar@wizzgeeks.com>
2025-03-05 16:21:19 +0530

    added additional schema for active time

--- Models/active_study_time.py
+++ Models/active_study_time.py
@@ -3,13 +3,18 @@ from Models.user_model import User
 from datetime import datetime
 
 
-class Active_user_details(Document):  
+class Active_study_time(Document):
     user = ReferenceField(User,required=True,reverse_delete_rule=2)
     date = DateTimeField(unique=True,default=datetime.strftime('%Y-%m-%d'))
+    in_time = DateTimeField(default=datetime.strftime('%H:%M %p'))
+    out_time = DateTimeField(default=datetime.strftime('%H:%M %p'))
+
 
     def to_json(self):
         return {
             "id":str(self.id),
             "user":str(self.user.id) if self.user else None,
-            "date": self.date
+            "date": self.date,
+            "in_time":self.in_time,
+            "out_time":self.out_time
         }
