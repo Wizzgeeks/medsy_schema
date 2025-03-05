58d3047b568818e6b83fa722b06719ed6cf000c2
naveenkumar <naveenkumar@wizzgeeks.com>
2025-03-05 19:03:43 +0530

    changes in schema

--- Models/active_study_time.py
+++ Models/active_study_time.py
@@ -5,14 +5,12 @@ from datetime import datetime
 class Active_study_time(Document):
     user = ReferenceField(User, required=True, reverse_delete_rule=2)
     date = StringField(unique=True,default=lambda: datetime.now().strftime('%d-%m-%Y'))
-    in_time = ListField(required=True)
-    out_time = ListField()
+    study_time = ListField(required=True)
 
     def to_json(self):
         return {
             "id": str(self.id),
             "user": str(self.user.id) if self.user else None,
             "date": self.date,
-            "in_time": self.in_time,
-            "out_time": self.out_time
+            "study_time": self.study_time,
         }
