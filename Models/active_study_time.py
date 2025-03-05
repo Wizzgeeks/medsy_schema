6494a329c7fa04aed95f7050fb4299468276a694
naveenkumar <naveenkumar@wizzgeeks.com>
2025-03-05 16:46:54 +0530

    changes in schema

--- Models/active_study_time.py
+++ Models/active_study_time.py
@@ -1,20 +1,18 @@
-from mongoengine import Document,ReferenceField,DateTimeField
+from mongoengine import Document, ReferenceField, StringField,ListField
 from Models.user_model import User
 from datetime import datetime
 
-
 class Active_study_time(Document):
-    user = ReferenceField(User,required=True,reverse_delete_rule=2)
-    date = DateTimeField(unique=True,default=datetime.strftime('%Y-%m-%d'))
-    in_time = DateTimeField(default=datetime.strftime('%H:%M %p'))
-    out_time = DateTimeField(default=datetime.strftime('%H:%M %p'))
-
+    user = ReferenceField(User, required=True, reverse_delete_rule=2)
+    date = StringField(unique=True,default=lambda: datetime.now().strftime('%d-%m-%Y'))
+    in_time = ListField(StringField(default=lambda: datetime.now().strftime('%I:%M %p')))
+    out_time = ListField(StringField(default=lambda: datetime.now().strftime('%I:%M %p')))
 
     def to_json(self):
         return {
-            "id":str(self.id),
-            "user":str(self.user.id) if self.user else None,
+            "id": str(self.id),
+            "user": str(self.user.id) if self.user else None,
             "date": self.date,
-            "in_time":self.in_time,
-            "out_time":self.out_time
+            "in_time": self.in_time,
+            "out_time": self.out_time
         }
--- Models/active_user_details.py
+++ Models/active_user_details.py
@@ -1,15 +1,14 @@
-from mongoengine import Document,ReferenceField,DateTimeField
+from mongoengine import Document, ReferenceField,StringField
 from Models.user_model import User
 from datetime import datetime
 
-
 class Active_user_details(Document):  
-    user = ReferenceField(User,required=True,reverse_delete_rule=2)
-    date = DateTimeField(unique=True,default=datetime.strftime('%Y-%m-%d'))
+    user = ReferenceField(User, required=True, reverse_delete_rule=2)
+    date = StringField(unique=True,default=lambda: datetime.now().strftime('%d-%m-%Y'))
 
     def to_json(self):
         return {
-            "id":str(self.id),
-            "user":str(self.user.id) if self.user else None,
-            "date": self.date
+            "id": str(self.id),
+            "user": str(self.user.id) if self.user else None,
+            "date": self.date if self.date else None
         }
