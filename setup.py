42f8b15cd49dde6e2b8b5d8eff9fdfbfe7dba605
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-03-18 15:35:43 +0530

    test_mcq_completion_status mark fields added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="medsy_schema",
-    version="1.5.7",
+    version="1.5.8",
     description="Schema for Flask Admin and User backends",
     author="wizzgeeks",
     author_email="",
