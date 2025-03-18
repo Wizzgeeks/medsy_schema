fde03f05e1987265ad490b14ed5dc849da759298
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-03-18 15:35:24 +0530

    test_mcq_completion_status mark fields added

--- Models/test_mcq_completion_status.py
+++ Models/test_mcq_completion_status.py
@@ -1,4 +1,4 @@
-from mongoengine import Document,ReferenceField,BooleanField
+from mongoengine import Document,ReferenceField,BooleanField,IntField
 from Models.user_model import User
 from Models.layer_1_model import Layer_1
 from Models.layer_2_model import Layer_2
@@ -11,8 +11,12 @@ class Test_completion_status(Document):
     layer2 = ReferenceField(Layer_2,reverse_delete_rule=2)
     layer3 = ReferenceField(Layer_3,reverse_delete_rule=2)
     completed=BooleanField(default=False)
+    total_marks=IntField()
+    scored_marks=IntField()
 
     def to_json(self):
         return {
             "completed":self.completed if self.completed else False,
+            "total_marks":self.total_marks,
+            "scored_marks":self.scored_marks
         }
