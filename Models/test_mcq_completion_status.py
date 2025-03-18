3f388b3e9b330267bfa7f069b39c6ebb47b15f9c
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-03-18 14:33:10 +0530

    test_mcq_completion_status model

---
+++ Models/test_mcq_completion_status.py
@@ -0,0 +1,18 @@
+from mongoengine import Document,ReferenceField,BooleanField
+from Models.user_model import User
+from Models.layer_1_model import Layer_1
+from Models.layer_2_model import Layer_2
+from Models.layer_3_model import Layer_3
+
+
+class Test_completion_status(Document):
+    user = ReferenceField(User,required=True,reverse_delete_rule=2)
+    layer1 = ReferenceField(Layer_1,reverse_delete_rule=2)
+    layer2 = ReferenceField(Layer_2,reverse_delete_rule=2)
+    layer3 = ReferenceField(Layer_3,reverse_delete_rule=2)
+    completed=BooleanField(default=False)
+
+    def to_json(self):
+        return {
+            "completed":self.completed if self.completed else False,
+        }
