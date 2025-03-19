e7c08c30094c3ca6dfe84c5db62a5475442f3b19
naveenkumar <naveenkumar@wizzgeeks.com>
2025-03-19 14:52:39 +0530

    added new schema for report a problem

---
+++ Models/problems_model.py
@@ -0,0 +1,20 @@
+from mongoengine import Document, ReferenceField, StringField
+from Models.user_model import User
+from datetime import datetime
+
+class Active_study_time(Document):
+    user = ReferenceField(User, required=True, reverse_delete_rule=2)
+    created_at = StringField(default=lambda: datetime.now().strftime('%d-%m-%Y'))
+    problem = StringField(required=True)
+    category = StringField(required=True)
+    location = StringField()
+
+    def to_json(self):
+        return {
+            "id": str(self.id),
+            "user": str(self.user.id) if self.user else None,
+            "created_at": self.created_at,
+            "location": self.location if self.location else None,
+            "category": self.category,
+            "problem": self.problem,
+        }
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="medsy_schema",
-    version="1.6.0",
+    version="1.6.1",
     description="Schema for Flask Admin and User backends",
     author="wizzgeeks",
     author_email="",
