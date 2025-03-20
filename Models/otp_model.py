3e4770954b1afea1009a3624b176488817554a6f
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-03-20 19:32:06 +0530

    otp checking model created

--- Models/otp_model.py
+++ Models/otp_model.py
@@ -1,6 +1,8 @@
-from mongoengine import  Document, StringField, IntField
+from mongoengine import  Document, StringField, IntField,DateTimeField
+from datetime import datetime
 
 
 class OTPModel(Document):
     phone = StringField(required=True, unique=True)
     otp = IntField()
+    exp=DateTimeField()
