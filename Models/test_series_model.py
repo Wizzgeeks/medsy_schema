829fe1589ec99abd5b5a139619c3e52f1946d077
naveenkumar <naveenkumar@wizzgeeks.com>
2025-03-26 11:44:45 +0530

    changes in schema

--- Models/test_series_model.py
+++ Models/test_series_model.py
@@ -1,43 +1,56 @@
-from mongoengine import Document,ReferenceField,ListField,StringField
+from mongoengine  import Document,ReferenceField,ListField,DictField
 from Models.course_model import Course
-from Models.layer_1_model import Layer_1
 from Models.subject_model import Subject
+from Models.layer_1_model import Layer_1
 from Models.layer_2_model import Layer_2
 from Models.layer_3_model import Layer_3
+from Models.layer1_page_model import Layer1_page
+from Models.layer2_page_model import Layer2_page
+from Models.layer3_page_model import Layer3_page
 from Models.year_model import Year
+from Models.prompt_content_model import Prompt_content
 
 class TestSeries(Document):
-    course = ReferenceField(Course,required=True,reverse_delete_rule=2)
-    year = ReferenceField(Year,required=True,reverse_delete_rule=2)
-    subject = ReferenceField(Subject,required=True,reverse_delete_rule=2)
-    layer1 = ReferenceField(Layer_1,reverse_delete_rule=2)
-    layer2 = ReferenceField(Layer_2,reverse_delete_rule=2)
-    layer3 = ReferenceField(Layer_3,reverse_delete_rule=2)
-    type = StringField()
-    test_series = ListField(required=True)
+    course=ReferenceField(Course,reverse_delete_rule=2,required=True)
+    year=ReferenceField(Year,reverse_delete_rule=2,required=True)
+    subject=ReferenceField(Subject,reverse_delete_rule=2)
+    layer1=ReferenceField(Layer_1,reverse_delete_rule=2)
+    layer2=ReferenceField(Layer_2,reverse_delete_rule=2)
+    layer3=ReferenceField(Layer_3,reverse_delete_rule=2)
+    layer1_page = ReferenceField(Layer1_page, reverse_delete_rule=2, null=True)
+    layer2_page = ReferenceField(Layer2_page, reverse_delete_rule=2, null=True)
+    layer3_page = ReferenceField(Layer3_page, reverse_delete_rule=2, null=True)
+    series=ListField(DictField())
+    prompt = ReferenceField(Prompt_content, reverse_delete_rule=2, required=True)
+
 
     def to_json(self):
         return {
             "id":str(self.id),
-            "course":str(self.course.id) if self.course else None,
-            "year":str(self.year.id) if self.year else None,
-            "subject":str(self.subject.id) if self.subject else None,
-            "layer1":str(self.layer1.id) if self.layer1 else None,
-            "layer2":str(self.layer2.id) if self.layer2 else None,
-            "layer3":str(self.layer3.id) if self.layer3 else None,
-            "type":self.type,
-            "test_series":self.test_series
+            'course':str(self.course.id),
+            'year': str(self.year.id) if self.year else None,
+            'subject':str(self.subject.id),
+            'layer1':str(self.layer1.id) if self.layer1 else None,
+            'layer2':str(self.layer2.id) if self.layer2 else None,
+            'layer3':str(self.layer3.id) if self.layer3 else None,
+            'series':self.series,
+            "layer1_page": str(self.layer1_page.id) if self.layer1_page else None,
+            "layer2_page": str(self.layer2_page.id) if self.layer2_page else None,
+            "layer3_page": str(self.layer3_page.id) if self.layer3_page else None,
+            "prompt": self.prompt.to_json() if self.prompt else None
         }
     
-    def with_key(self):
+    def to_user(self):
         return {
-            "id":str(self.id),
-            "course":self.course.to_json() if self.course else None,
-            "year":self.year.to_json() if self.year else None,
-            "subject":self.subject.to_json() if self.subject else None,
-            "layer1":self.layer1.to_json() if self.layer1 else None,
-            "layer2":self.layer2.to_json() if self.layer2 else None,
-            "layer3":self.layer3.to_json() if self.layer3 else None,
-            "type":self.type,
-            "test_series":self.test_series
+            "id": str(self.id),
+            'course': str(self.course.id) if self.course else None,
+            'year': str(self.year.id) if self.year else None,
+            "subject": str(self.subject.id) if self.subject else None,
+            "layer1": str(self.layer1.id) if self.layer1 else None,
+            "layer2": str(self.layer2.id) if self.layer2 else None,
+            "layer3": str(self.layer3.id) if self.layer3 else None,
+            "layer1_page": str(self.layer1_page.id) if self.layer1_page else None,
+            "layer2_page": str(self.layer2_page.id) if self.layer2_page else None,
+            "layer3_page": str(self.layer3_page.id) if self.layer3_page else None,
+            'series':self.series,
         }
