6bdc3f98dd577b8efae678a22add0d597259cb28
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-04-02 16:02:24 +0530

    timespent new field added

--- Models/layer3_time_spent.py
+++ Models/layer3_time_spent.py
@@ -8,7 +8,7 @@ from Models.layer_3_model import Layer_3
 from Models.layer3_page_model import Layer3_page
 
 class Layer3_time_spent(Document):
-    coures = ReferenceField(Course,required=True,reverse_delete_rule=2)
+    course = ReferenceField(Course,required=True,reverse_delete_rule=2)
     subject = ReferenceField(Subject,required=True,reverse_delete_rule=2)
     layer1 = ReferenceField(Layer_1,reverse_delete_rule=2)
     layer2=ReferenceField(Layer_2,required=True,reverse_delete_rule=2)
@@ -24,10 +24,10 @@ class Layer3_time_spent(Document):
         return {
             # "layer1":str(self.layer1.id) if self.layer1 else None,
             # "user":str(self.user.id) if self.user else None,
-            # "coures":str(self.coures.id) if self.coures else None,
+            # "course":str(self.course.id) if self.course else None,
             # "subject":str(self.subject.id) if self.subject else None,
             "user":self.user.to_json() if self.user else None,
-            "coures":self.coures.to_json() if self.coures else None,
+            "course":self.course.to_json() if self.course else None,
             "subject":self.subject.to_json() if self.subject else None,
             "layer1":self.layer1.to_json() if self.layer1 else None,
             "layer2":self.layer2.to_json() if self.layer2 else None,
