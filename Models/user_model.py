9172572c599a117dbac90a394199d0a7a1f100cb
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-04-09 16:31:34 +0530

    timezone changed

--- Models/user_model.py
+++ Models/user_model.py
@@ -1,5 +1,5 @@
 from mongoengine import Document, StringField,EmailField,BooleanField
-import datetime,timezone
+from datetime import datetime,timezone
 class User(Document):
     course =StringField()
     year = StringField()
