dcafb3d91d0b725565060e8045d045167f23947a
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-04-09 12:29:54 +0530

    new emum value added in dependent_component

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="medsy_schema",
-    version="2.3.5",
+    version="2.3.6",
     description="Schema for Flask Admin and User backends",
     author="wizzgeeks",
     author_email="",
