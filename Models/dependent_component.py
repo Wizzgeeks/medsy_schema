cf3a442cc2c55b0c7642a7d06d8e2b49cf8fa62e
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-04-14 18:04:48 +0530

    prompt id field removed

--- Models/dependent_component.py
+++ Models/dependent_component.py
@@ -3,7 +3,6 @@ from Models.prompt_model import Prompt
 
 class Dependent_components(Document):
     name=StringField()
-    prompt=ReferenceField(Prompt,required=True,reverse_delete_rule=2)
     types=StringField(required=True,unique=True)
 
     def update(self, **kwargs):
@@ -13,7 +12,6 @@ class Dependent_components(Document):
     def to_json(self):
         return {
             "name":self.name,
-            "prompt":str(self.prompt.id),
             "types":self.types
         }
     
