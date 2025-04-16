887b2bdfafcd5331f692cc5c8e0f65b93df01e81
naveenkumar <naveenkumar@wizzgeeks.com>
2025-04-16 12:45:54 +0530

    changes

--- Models/coin_model.py
+++ Models/coin_model.py
@@ -1,23 +1,31 @@
-from mongoengine import Document,ReferenceField,IntField
+from mongoengine import Document,ReferenceField,IntField,StringField
 from Models.course_model import Course
 from Models.user_model import User
+from datetime import datetime,timezone
 
 class Coin(Document):
     course = ReferenceField(Course,required=True,reverse_delete_rule=2)
     user = ReferenceField(User,required=True,reverse_delete_rule=2)
-    count = IntField(required=True)
+    coins = IntField(required=True)
+    action = StringField(required=True)
+    date = StringField(default=lambda: datetime.now(timezone.utc).strftime('%d-%m-%Y')) 
+
 
     def to_json(self):
         return {
             "id":str(self.id),
             "course":str(self.course.id) if self.course else None,
             "user":str(self.user.id) if self.user else None,
-            "count":self.count
+            "coins":self.coins,
+            "action":self.action,
+            "date":self.date
         }
     def with_key(self):
         return {
             "id":str(self.id),
             "course":self.course.to_json() if self.course else None,
             "user":self.user.to_json() if self.user else None,
-            "count":self.count
+            "coins":self.coins,
+            "action":self.action,
+            "date":self.date
         }
