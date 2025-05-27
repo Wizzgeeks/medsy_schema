2b8b9888f1e9303eee651c8ba49ee8140453cb67
naveenkumar <naveenkumar@wizzgeeks.com>
2025-05-27 16:53:22 +0530

    changes

---
+++ Models/exam_result.py
@@ -0,0 +1,44 @@
+from mongoengine  import Document,ReferenceField,ListField,DictField,GenericReferenceField
+from Models.course_model import Course
+from Models.subject_model import Subject
+from Models.layer_1_model import Layer_1
+from Models.layer_2_model import Layer_2
+from Models.layer_3_model import Layer_3
+from Models.year_model import Year
+
+class Exam_results(Document):
+    course=ReferenceField(Course,reverse_delete_rule=2,required=True)
+    year=ReferenceField(Year,reverse_delete_rule=2,required=True)
+    subject=ReferenceField(Subject,reverse_delete_rule=2)
+    layer1=ReferenceField(Layer_1,reverse_delete_rule=2)
+    layer2=ReferenceField(Layer_2,reverse_delete_rule=2)
+    layer3=ReferenceField(Layer_3,reverse_delete_rule=2)
+    page = GenericReferenceField(required=True)
+    result=ListField(DictField())
+
+
+    def to_json(self):
+        return {
+            "id":str(self.id),
+            'course':str(self.course.id),
+            'year': str(self.year.id) if self.year else None,
+            'subject':str(self.subject.id),
+            'layer1':str(self.layer1.id) if self.layer1 else None,
+            'layer2':str(self.layer2.id) if self.layer2 else None,
+            'layer3':str(self.layer3.id) if self.layer3 else None,
+            'result':self.result,
+            "page": str(self.page.id) if self.page else None,
+        }
+    
+    def to_user(self):
+        return {
+            "id": str(self.id),
+            'course': str(self.course.id) if self.course else None,
+            'year': str(self.year.id) if self.year else None,
+            "subject": str(self.subject.id) if self.subject else None,
+            "layer1": str(self.layer1.id) if self.layer1 else None,
+            "layer2": str(self.layer2.id) if self.layer2 else None,
+            "layer3": str(self.layer3.id) if self.layer3 else None,
+            "page": str(self.page.id) if self.page else None,
+            'result':self.result,
+        }
