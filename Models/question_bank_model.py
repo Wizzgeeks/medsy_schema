405656eeb3a852d24f3f1ff40e5bf9281b6a1110
naveen <naveenkumar@wizzgeeks.com>
2025-06-10 17:58:58 +0530

    changes in schema

--- Models/question_bank_model.py
+++ Models/question_bank_model.py
@@ -56,7 +56,7 @@ class Question_bank(Document):
             'layer1':str(self.layer1.id) if self.layer1 else None,
             'layer2':str(self.layer2.id) if self.layer2 else None,
             'layer3':str(self.layer3.id) if self.layer3 else None,
-            "mcq": [q.to_dict() for q in self.mcq],
+            "questions": [q.to_dict() for q in self.questions],
             "layer1_page": str(self.layer1_page.id) if self.layer1_page else None,
             "layer2_page": str(self.layer2_page.id) if self.layer2_page else None,
             "layer3_page": str(self.layer3_page.id) if self.layer3_page else None,
@@ -77,5 +77,5 @@ class Question_bank(Document):
             "layer2_page": str(self.layer2_page.id) if self.layer2_page else None,
             "layer3_page": str(self.layer3_page.id) if self.layer3_page else None,
             "subject_page":str(self.subject_page.id) if self.subject_page else None,
-            "mcq": [q.to_dict() for q in self.mcq],
+            "questions": [q.to_dict() for q in self.questions],
         }
