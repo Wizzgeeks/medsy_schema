b8890328441b5c396a8ce448b73b6183e75cd0ff
Naveenkumar <naveenkumar@wizzgeeks.com>
2025-06-23 17:51:56 +0530

    changes in schema

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="medsy_schema",
-    version="3.6.3",
+    version="3.6.4",
     description="Schema for Flask Admin and User backends",
     author="wizzgeeks",
     author_email="",
