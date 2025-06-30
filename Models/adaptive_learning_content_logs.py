efdb9041048f63e6a424cc46ed66a79d3c9eb72e
Naveenkumar <naveenkumar@wizzgeeks.com>
2025-06-30 12:50:06 +0530

    changes in schema

--- Models/adaptive_learning_content_logs.py
+++ Models/adaptive_learning_content_logs.py
@@ -4,10 +4,6 @@ from Models.subject_model import Subject
 from Models.layer_1_model import Layer_1
 from Models.layer_2_model import Layer_2
 from Models.layer_3_model import Layer_3
-from Models.layer1_page_model import Layer1_page
-from Models.layer2_page_model import Layer2_page
-from Models.layer3_page_model import Layer3_page
-from Models.subject_page_model import Subject_page
 from Models.year_model import Year
 from datetime import datetime,timezone
 from Models.adaptive_page_content import Adaptive_learning_content
@@ -23,10 +19,7 @@ class Adaptive_learning_content_logs(Document):
     layer1 = ReferenceField(Layer_1,reverse_delete_rule=2)
     layer2 = ReferenceField(Layer_2,reverse_delete_rule=2)
     layer3 = ReferenceField(Layer_3,reverse_delete_rule=2)
-    layer1_page = ReferenceField(Layer1_page, reverse_delete_rule=2, null=True)
-    layer2_page = ReferenceField(Layer2_page, reverse_delete_rule=2, null=True)
-    layer3_page = ReferenceField(Layer3_page, reverse_delete_rule=2, null=True)
-    subject_page=ReferenceField(Subject_page,reverse_delete_rule=2, null=True)
+    layer = StringField(choices=['subject', 'layer1', 'layer2', 'layer3'], required=True)
     logs=ListField(DictField())
     conversation = ListField(default=[])
     created_at=DateTimeField(default=datetime.now(timezone.utc))
@@ -44,10 +37,7 @@ class Adaptive_learning_content_logs(Document):
             'layer1':str(self.layer1.id) if self.layer1 else None,
             'layer2':str(self.layer2.id) if self.layer2 else None,
             'layer3':str(self.layer3.id) if self.layer3 else None,
-            "layer1_page": str(self.layer1_page.id) if self.layer1_page else None,
-            "layer2_page": str(self.layer2_page.id) if self.layer2_page else None,
-            "layer3_page": str(self.layer3_page.id) if self.layer3_page else None,
-            "subject_page":str(self.subject_page.id) if self.subject_page else None,
+            "layer":self.layer if self.layer else None,
             'status':self.status,
             'logs_content':self.logs,
             'logs_mcq':self.logs,
