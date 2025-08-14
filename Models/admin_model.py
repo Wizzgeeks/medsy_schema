8064cded1427c46f576217565578a29b0c632f57
Naveenkumar <naveenkumar@wizzgeeks.com>
2025-08-14 12:13:52 +0530

    changes in schema

--- Models/admin_model.py
+++ Models/admin_model.py
@@ -1,9 +1,10 @@
-from mongoengine import Document, StringField,EmailField,ReferenceField,IntField,ListField,DateTimeField
+from mongoengine import Document, StringField,EmailField,ReferenceField,IntField,ListField,DateTimeField,BooleanField
 
 from datetime import datetime,timezone
 from Models.institution_model import Institution
 from Models.university_model import University
 from Models.course_model import Course
+from Models.year_model import Year
 from Models.subject_model import Subject
 
 class Admin(Document):
@@ -19,9 +20,11 @@ class Admin(Document):
     university = ListField(ReferenceField(University))
     institution = ListField(ReferenceField(Institution))
     course = ListField(ReferenceField(Course))
+    year = ListField(ReferenceField(Year))
     subject = ListField(ReferenceField(Subject))
     section = ListField(StringField())
     status = StringField()
+    active = BooleanField(default=True)
     created_at = DateTimeField(default=datetime.now(timezone.utc))
     auth_token=StringField()
 
@@ -39,11 +42,14 @@ class Admin(Document):
             'university': [str(u.id) for u in self.university],
             'institution': [str(i.id) for i in self.institution],
             'course': [str(c.id) for c in self.course],
+            'year': [str(y.id) for y in self.year],
             'subject': [str(s.id) for s in self.subject],
             'section': self.section,
             'status': self.status,
+            'active': self.active,
             'created_at': self.created_at.isoformat() if self.created_at else None,
-            'auth_token': self.auth_token
+            'auth_token': self.auth_token,
+            
         }
     def remove_expired_tokens(self):
         current_time = datetime.now(timezone.utc)
--- Models/permission_model.py
+++ Models/permission_model.py
@@ -1,5 +1,19 @@
-from mongoengine import Document, StringField, DictField
+import datetime
+from mongoengine import Document, StringField, DictField,BooleanField,DateTimeField
 
 class AdminPermission(Document):
-    role = StringField(required=True)
+    role = StringField(required=True,unique=True)
     module_permissions = DictField()
+    active = BooleanField(default=True) 
+    created_at = DateTimeField(default=datetime.now(datetime.timezone.utc))
+
+
+    def to_json(self):
+        return {
+            'id': str(self.id),
+            'role': self.role,
+            'module_permissions': self.module_permissions,
+            'active': self.active,
+            'created_at': self.created_at.isoformat() if self.created_at else None,
+        }
+
