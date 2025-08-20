6de1554dc88bb3d93d73180d4219791ecc15db27
Naveenkumar <naveenkumar@wizzgeeks.com>
2025-08-20 11:05:34 +0530

    changes in new schema setup

--- Models/user_model.py
+++ Models/user_model.py
@@ -11,15 +11,15 @@ class User(Document):
     institution_id = ReferenceField(Institution, required=True, reverse_delete_rule=CASCADE)
     course_id = ReferenceField(Course, required=True, reverse_delete_rule=CASCADE)
     year_id = ReferenceField(Year, required=True, reverse_delete_rule=CASCADE)
+    user_id = StringField(required=True, unique=True)
     university = StringField(required=True)
     institutions = StringField(required=True)
-    user_id = StringField(required=True, unique=True)
-    section = StringField(required=True)
     course =StringField(required=True)
     year = StringField(required=True)
+    section = StringField(required=True)
     username = StringField(required=True)
     profile=StringField()
-    role=StringField(choices=['student','admin','superadmin'],required=True)
+    role=StringField(choices=['student'],required=True)
     auth_token = StringField()
     password=StringField()
     institution=StringField()
@@ -37,12 +37,7 @@ class User(Document):
     def update(self, **kwargs):
         self.clean()
         return super().update(**kwargs)
-    # def clean(self):
-    #     if not re.match(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', self.email):
-    #         raise ValueError("Enter a Valid mail")
-    #     if self.role == 'user':
-    #         if not self.course:
-    #             raise ValueError("Please select a course")
+    
     def to_json(self):
         return {
             "id": str(self.id),
@@ -77,9 +72,14 @@ class User(Document):
             "id": str(self.id),
             "university_id": str(self.university_id.id) if self.university_id else None,
             "institution_id": str(self.institution_id.id) if self.institution_id else None,
-            "section_id": str(self.section_id.id) if self.section_id else None,
             "course_id": str(self.course_id.id) if self.course_id else None,
             "year_id": str(self.year_id.id) if self.year_id else None,
+            "user_id": self.user_id if self.user_id else None,
+            "university": self.university if self.university else None,
+            "institutions": self.institutions if self.institutions else None,
+            "section": self.section if self.section else None,
+            "course": (self.course) if self.course else None,
+            "year": (self.year) if self.year else None,
             "username": self.username,
             "profile":self.profile if self.profile else None,
             "role":self.role if self.role else None,
