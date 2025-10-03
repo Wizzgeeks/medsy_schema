5092c0c920b7800b265ae9928f2ee248d0ae03da
Naveenkumar <naveenkumar@wizzgeeks.com>
2025-10-03 17:42:40 +0530

    changes in schema

--- Models/admin_onboard.py
+++ Models/admin_onboard.py
@@ -4,6 +4,8 @@ from datetime import datetime,timezone
 
 class Admin_Onboard(Document):
     name = StringField(required=True)
+    phone_country = StringField(required=True, default="IND")
+    country_code = StringField(required=True, default="+91")
     phone = StringField(required=True, unique=True)
     active = StringField(default=True)
     created_at = DateTimeField(default=datetime.now(timezone.utc))
