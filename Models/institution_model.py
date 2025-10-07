6959e1ecb019a09a0f9238db817b8befd91ae102
Naveenkumar <naveenkumar@wizzgeeks.com>
2025-10-07 14:16:26 +0530

    changes in api

--- Models/institution_model.py
+++ Models/institution_model.py
@@ -65,3 +65,35 @@ class Institution(Document):
             'onboarded_at': self.onboarded_at.strftime('%d/%m/%Y') if self.onboarded_at else None,
 
         }
+
+    def onboard_json(self):
+        return {
+            'id': str(self.id),
+            'name': self.name,
+            'type': self.type,
+            'address': self.address if self.address else None,
+            'university_id': self.university.to_json() if self.university else None,
+            'key': self.key if self.key else None,
+            # 'icon': self.icon if self.icon else None,
+            # 'cover_image': self.cover_image if self.cover_image else None,
+            # 'about_us': self.about_us if self.about_us else None,
+            # 'website_url': self.website_url,
+            'created_at': self.created_at.strftime('%d/%m/%Y') if self.created_at else None,
+            # 'phone_country': self.phone_country if self.phone_country else None,
+            # 'country_code': self.country_code if self.country_code else None,
+            # 'phone': self.phone if self.phone else None,
+            # 'email': self.email if self.email else None,
+            'username1': self.username1 if self.username1 else None,
+            'phone_country1': self.phone_country1 if self.phone_country1 else None,
+            'country_code1': self.country_code1 if self.country_code1 else None,
+            'phone1': self.phone1 if self.phone1 else None,
+            'email1': self.email1 if self.email1 else None,
+            'username2': self.username2 if self.username2 else None,
+            'phone_country2': self.phone_country2 if self.phone_country2 else None,
+            'country_code2': self.country_code2 if self.country_code2 else None,
+            'phone2': self.phone2 if self.phone2 else None, 
+            'email2': self.email2 if self.email2 else None,
+            'onboarded': self.onboarded if self.onboarded else False,
+            'onboarded_at': self.onboarded_at.strftime('%d/%m/%Y') if self.onboarded_at else None,
+
+        }
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="medsy_schema",
-    version="4.7.1",
+    version="4.7.8",
     description="Schema for Flask Admin and User backends",
     author="wizzgeeks",
     author_email="",
