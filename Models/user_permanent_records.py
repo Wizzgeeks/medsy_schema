5a192299fcd3887970854fcba842e3612c925905
Naveenkumar <naveenkumar@wizzgeeks.com>
2026-01-05 11:53:28 +0530

    changes in api

--- Models/user_permanent_records.py
+++ Models/user_permanent_records.py
@@ -1,20 +1,16 @@
-from mongoengine import Document,DateTimeField,StringField
-from datetime import datetime,timezone
-
+from mongoengine import Document, DateTimeField, StringField
+from datetime import datetime, timezone
 
 class UserRecords(Document):
-    email = StringField()
+    email_hash = StringField(unique=True, sparse=True)
+    phone_hash = StringField(unique=True, sparse=True)
     phone_country = StringField()
     country_code = StringField()
-    phone_no = StringField()
     created_at = DateTimeField(default=datetime.now(timezone.utc))
-    
-    def to_json(self):
-        return {
-            "id": str(self.id),
-            "email":self.email if self.email else None,
-            "phone_country":self.phone_country if self.phone_country else None,
-            "country_code":self.country_code if self.country_code else None,
-            "phone_no":self.phone_no if self.phone_no else None,
-            'created_at':str(self.created_at) if self.created_at else None,
-        }
+
+    meta = {
+        "indexes": [
+            "email_hash",
+            "phone_hash"
+        ]
+    }
