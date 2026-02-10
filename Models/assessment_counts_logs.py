0976c85bfd94a4d0b5fa2802920b44a3013cd374
Naveenkumar <naveenkumar@wizzgeeks.com>
2026-02-10 12:01:49 +0530

    changes in api

---
+++ Models/assessment_counts_logs.py
@@ -0,0 +1,27 @@
+from mongoengine import ( Document, StringField, ReferenceField, DateTimeField, CASCADE,DictField)
+from datetime import datetime,timezone
+from Models.user_model import User
+
+class AssessmentCountsLogs(Document):
+    user = ReferenceField(User,reverse_delete_rule=CASCADE,required=True)
+    logs_data = DictField()
+    additional_data = DictField()
+    assessment_counts = DictField()
+    assessment_new_counts = DictField()
+    remarks = StringField()
+    created_at = DateTimeField(default=lambda: datetime.now(timezone.utc))
+    
+
+    def to_json(self):
+        return {
+            "id": str(self.id),
+            "user": {"id": str(self.user.id),"name": self.user.username} if self.user else {},
+            "logs_data": self.logs_data,
+            "additional_data": self.additional_data,
+            "assessment_counts": self.assessment_counts,
+            "assessment_new_counts": self.assessment_new_counts,
+            "remarks": self.remarks,
+            "created_at": self.created_at.strftime("%I:%M %p %b %d, %Y").lower() if self.created_at else None,
+        }
+        
+    
